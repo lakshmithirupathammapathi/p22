dad116bdb6c999f1fef07f42e8257fa81539b34e
lakshmithirupathammapathi <41730938+lakshmithirupathammapathi@users.noreply.github.com>
2018-09-10 09:45:59 +0530

    Create p22.java

---
+++ p22.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int n ;
+		long factorial=1;
+		Scanner sc=new Scanner(System.in);
+		n=sc.nextInt();
+		for(int i=1;i<=n;++i)
+		{
+			factorial=factorial*i;
+		}
+		System.out.println(factorial);
+		
+		
+	}
+	
+}
